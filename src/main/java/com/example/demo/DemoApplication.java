928744f142101def00d7921312ec64d2536a2e7b
root <root@k8s-test-master.int.cloudsmartz.net>
2026-03-09 12:00:07 +0000

    java microservice

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Ravinder! Java Microservice running on Kubernetes.";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
